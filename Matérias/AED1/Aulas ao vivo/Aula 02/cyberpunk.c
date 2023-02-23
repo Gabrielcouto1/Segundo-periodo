582bf7d6a829d4292a0b45da3338843c9f8f73cf
Gabriel Couto <gabrielcouto3.14@gmail.com>
2023-02-23 16:37:26 -0300

    main commit

--- Matérias/AED1/Aulas ao vivo/Aula 02/cyberpunk.c
+++ Matérias/AED1/Aulas ao vivo/Aula 02/cyberpunk.c
@@ -1,28 +1,21 @@
-/*
-
-
-    https://github.com/gabrielcouto1
-*/
-
 #include <stdio.h> 
 #include <stdlib.h> 
-#include <time.h> 
-#include <ctype.h> 
-#include <string.h> 
-#include <math.h> 
+#include <string.h>
 
-int main(int argc, char *argv[])
-{
-    FILE *F = NULL;
-    int i;
-    F=fopen("cyberpunk.txt","w");
-    
-    fprintf(F,"Eu tenho uma dúvida sobre Cyberpunk 2077.\nEu preciso jogar ");
-    for (i=1;i<=2077;i++){
-        fprintf(F,"Cyberpunk %d, ",i);
+int main(int argc, char *argv[]){
+    for(int j=1;j<2077;j++){
+        char nome[50];
+        sprintf(nome,"Cyberpunk%d.txt",j);
+        FILE *fp;
+        fp=fopen(nome, "w+");
+        fprintf(fp,"Eu tenho uma dúvida sobre Cyberpunk 2077.\nEu preciso jogar ");
+        for (int i=1;i<2077;i++){
+            fprintf(fp,"Cyberpunk %d, ",i);
+        }
+        fprintf(fp,"para poder jogar o cyberpunk 2077?\n");
+        fclose(fp);
     }
-    fprintf(F,"para poder jogar o cyberpunk 2077?\n");
-
-    fclose(F);
-    return 0;  //Encerra o programa e retorna o valor 0
+    system("mkdir Cyberpunk");
+    system("move *.txt Cyberpunk");
+    return 0;
 }
